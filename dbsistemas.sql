b2c8fc02bc6d8826238fd207d6ad07f96b76292f
GuelserAlexanderPaizFlores <gpaizf@miumg.edu.gt>
2020-04-21 11:55:19 -0600

    Login y DB
    se creo el login y se subio la base de datos dbsistemas completa

--- dbsistemas.sql
+++ dbsistemas.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 20-04-2020 a las 05:43:02
+-- Tiempo de generación: 21-04-2020 a las 15:40:30
 -- Versión del servidor: 10.4.11-MariaDB
 -- Versión de PHP: 7.2.27
 
